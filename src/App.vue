b6dd1c4735d62a896ca85a347a16f6c964777e89
yangzehui <zehuimeng@gmail.com>
2021-10-01 02:37:05 +0800

    第一次创建组件

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <LeftSideBar/>
+  </div>
+</template>
+
+<script>
+import LeftSideBar from './components/LeftSideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LeftSideBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/LeftSideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-radio-group v-model="tabIndex" style="margin-bottom: 30px">
+    <el-radio-button label="file">file</el-radio-button>
+    <el-radio-button label="protol">protol</el-radio-button>
+    <el-radio-button label="code">代码分析</el-radio-button>
+    <el-radio-button label="fault">故障分析</el-radio-button>
+  </el-radio-group>
+
+  <el-tabs tab-position="left" style="height: 200px" v-show="tabIndex === 'file'">
+    <el-tab-pane label="文件拼接"></el-tab-pane>
+    <el-tab-pane label="文件属性"></el-tab-pane>
+    <el-tab-pane label="文件转换"></el-tab-pane>
+  </el-tabs>
+  <el-tabs tab-position="left" style="height: 400px" v-show="tabIndex === 'protol'">
+    <el-tab-pane label="新CAN"></el-tab-pane>
+    <el-tab-pane label="老CAN"></el-tab-pane>
+    <el-tab-pane label="MB"></el-tab-pane>
+    <el-tab-pane label="主从"></el-tab-pane>
+    <el-tab-pane label="TCP-MB"></el-tab-pane>
+    <el-tab-pane label="BIN"></el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tabIndex: 'file',
+    }
+  },
+}
+</script>
+
